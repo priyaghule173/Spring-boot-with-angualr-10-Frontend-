7c88eca1344a2ebd230d6abe373d99571008d59e
priya ghule <priyaghule173@gmail.com>
2021-07-10 18:28:53 +0530

    spring backend crud

---
+++ src/main/java/com/priya/spring/backend/controller/EmployeeController.java
@@ -0,0 +1,72 @@
+package com.priya.spring.backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.priya.spring.backend.exception.ResourceNotFoundException;
+import com.priya.spring.backend.model.Employee;
+import com.priya.spring.backend.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all Employees
+	@CrossOrigin
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+		
+	}
+	
+	//create employee rest api
+	@CrossOrigin
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	//get Employee by id rest api
+	@CrossOrigin
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+	.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" +id));
+		return ResponseEntity.ok(employee) ;
+		
+	}
+	
+	//update employee by id rest api	
+	@CrossOrigin
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee1 = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" +id));
+		System.out.print(employee1);
+		
+		employee1.setFirstName(employeeDetails.getFirstName());
+		employee1.setLastName(employeeDetails.getLastName());
+		employee1.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedEmployee=employeeRepository.save(employee1);
+//		
+	return ResponseEntity.ok(updatedEmployee);
+		
+	}
+	
+}
---
+++ src/main/java/com/priya/spring/backend/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.priya.spring.backend.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.priya.spring.backend.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+	
+}
