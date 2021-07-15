222a2616baaee5a2cda574e2685fc41dc8ba89eb
priya ghule <priyaghule173@gmail.com>
2021-07-15 17:56:25 +0530

    priya

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -19,12 +19,15 @@ export class EmployeeService {
   createEmployee(employee: Employee): Observable<Object>{
     return this.httpClient.post(`${this.baseURL}`, employee);
   }
-  getEmployeeById(id: number): Observable<Employee>{
 
+  getEmployeeById(id: number): Observable<Employee>{
     return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
   }
 
   updateEmployee(id:number , employee: Employee): Observable<Object>{
     return this.httpClient.put(`${this.baseURL}/${id}`, employee);
+  }
+  deleteEmployee(id:number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
 }
